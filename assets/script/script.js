f1c9673f4b7bcc7ae67a10c6ea72775902a04f62
BenD2525 <bendawson2525@gmail.com>
2022-04-09 11:30:02 +0000

    Add media queries

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,10 +17,10 @@ setInterval(function timer() {
     let timer= document.getElementById('timer-box')
     let seconds= totalSeconds - (minute * 60);
         if (seconds < 10) {
-            timer.innerHTML= `Timer: ${minute}:0${seconds}`;
+            timer.innerText= `Timer: ${minute}:0${seconds}`;
         }
         else {
-    timer.innerHTML= `Timer: ${minute}:${seconds}`;
+    timer.innerText= `Timer: ${minute}:${seconds}`;
         };
 },1000);
 
@@ -102,44 +102,44 @@ function changeQuestion() {
 
 function displayQuestion() {
     for (question of questions) {
-    document.getElementById('answer-one').innerHTML= questions[0].answer[1];
-    document.getElementById('answer-two').innerHTML= questions[0].answer[2];
-    document.getElementById('answer-three').innerHTML= questions[0].answer[3];
-    document.getElementById('answer-four').innerHTML= questions[0].answer[4];
+    document.getElementById('answer-one').innerText= questions[0].answer[1];
+    document.getElementById('answer-two').innerText= questions[0].answer[2];
+    document.getElementById('answer-three').innerText= questions[0].answer[3];
+    document.getElementById('answer-four').innerText= questions[0].answer[4];
     }
 };
 
    /* COMMENTED OUT let question = 1
 
     if (question == 1) {
-        document.getElementById('answer-one').innerHTML = "China"
-        document.getElementById('answer-two').innerHTML = "India"
-        document.getElementById('answer-three').innerHTML = "Egypt"
-        document.getElementById('answer-four').innerHTML = "Rwanda"
+        document.getElementById('answer-one').innerText = "China"
+        document.getElementById('answer-two').innerText = "India"
+        document.getElementById('answer-three').innerText = "Egypt"
+        document.getElementById('answer-four').innerText = "Rwanda"
     }
     else if (question == 2) {
-        document.getElementById('answer-one').innerHTML = "Argentina"
-        document.getElementById('answer-two').innerHTML = "Cameroon"
-        document.getElementById('answer-three').innerHTML = "Gabon"
-        document.getElementById('answer-four').innerHTML = "Ireland"
+        document.getElementById('answer-one').innerText = "Argentina"
+        document.getElementById('answer-two').innerText = "Cameroon"
+        document.getElementById('answer-three').innerText = "Gabon"
+        document.getElementById('answer-four').innerText = "Ireland"
     }
     else if (question == 3) {
-        document.getElementById('answer-one').innerHTML = "Scotland"
-        document.getElementById('answer-two').innerHTML = "Germany"
-        document.getElementById('answer-three').innerHTML = "Belgium"
-        document.getElementById('answer-four').innerHTML = "Netherlands"
+        document.getElementById('answer-one').innerText = "Scotland"
+        document.getElementById('answer-two').innerText = "Germany"
+        document.getElementById('answer-three').innerText = "Belgium"
+        document.getElementById('answer-four').innerText = "Netherlands"
     }
     else if (question == 4) {
-        document.getElementById('answer-one').innerHTML = "New Zealand"
-        document.getElementById('answer-two').innerHTML = "Australia"
-        document.getElementById('answer-three').innerHTML = "Peru"
-        document.getElementById('answer-four').innerHTML = "Tonga"
+        document.getElementById('answer-one').innerText = "New Zealand"
+        document.getElementById('answer-two').innerText = "Australia"
+        document.getElementById('answer-three').innerText = "Peru"
+        document.getElementById('answer-four').innerText = "Tonga"
     }
     else if (question == 5) {
-        document.getElementById('answer-one').innerHTML = "Spain"
-        document.getElementById('answer-two').innerHTML = "Portugal"
-        document.getElementById('answer-three').innerHTML = "Greece"
-        document.getElementById('answer-four').innerHTML = "Moldova"
+        document.getElementById('answer-one').innerText = "Spain"
+        document.getElementById('answer-two').innerText = "Portugal"
+        document.getElementById('answer-three').innerText = "Greece"
+        document.getElementById('answer-four').innerText = "Moldova"
     }
 };
 */
