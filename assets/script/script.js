e235fb1d6dd5490986fcd89343863595685a2cfc
BenD2525 <bendawson2525@gmail.com>
2022-04-19 18:14:21 +0000

    Attempt to add modal

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,11 +5,47 @@ const buttons = Array.from(document.querySelectorAll(".answer"));
 const scoreEl = document.querySelector("#score-section");
 const wrapper = document.querySelector(".wrapper");
 const navBar = document.querySelectorAll(".nav");
+const openModalButtons= document.querySelectorAll('[data-modal-target]');
+const closeModalButtons= document.querySelectorAll('[data-close-button]');
+const overlay= document.getElementById('overlay');
 let questionId;
 let userCanAnswer;
 let currentQuestion;
 let score = 0;
 let game;
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal= document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal
+    }) 
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal= button.closest('modal')
+        closeModal(modal)
+    })
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+};
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+};
 
 // Burger Menu
 
