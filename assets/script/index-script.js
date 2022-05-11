087099d50725bd6acbc62fdf51da0d3f66dcfbbb
BenD2525 <bendawson2525@gmail.com>
2022-05-11 08:07:34 +0000

    Add comments to index-script and add event listeners for burger menu

--- assets/script/index-script.js
+++ assets/script/index-script.js
@@ -5,14 +5,17 @@ const openModalButtons= document.querySelectorAll('[data-modal-target');
 const closeModalButtons= document.querySelectorAll('[data-close-button]');
 const overlay= document.getElementById('overlay');
 const modalButtons = Array.from(document.querySelectorAll(".modal-button"));
+const burgerMenu= document.getElementById("toggle");
+const menuItems= document.getElementsByClassName("menu-item");
 
+//Add event listener to modal buttons
 openModalButtons.forEach(modal => {
     modal.addEventListener('click', () => {
         openModal(modal)
     })
 });
 
-
+//Add event listener to overlay
 overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active');
     modals.forEach(modal => {
@@ -20,6 +23,7 @@ overlay.addEventListener('click', () => {
     }) 
 });
 
+//Add event listener to close modal buttons
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal= button.closest('.modal');
@@ -27,6 +31,7 @@ closeModalButtons.forEach(button => {
     })
 });
 
+//Open modal button
 function openModal(modal) {
     modal= modal.nextElementSibling
     if (modal == null) return
@@ -34,13 +39,14 @@ function openModal(modal) {
     overlay.classList.add('active')
 };
 
+//Close modal button
 function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
 };
 
-// Burger Menu
+// Works out whether menu is open or not and provides a boolean
 const navIsOpen = function() {
     if (wrapper.classList.contains ("nav-open")) {
         return true;
@@ -49,10 +55,14 @@ const navIsOpen = function() {
         return false;
     }
 };
+
+// Adds open class to nav bar and removes closed class
 const openNav = () => {
     wrapper.classList.add("nav-open");
     wrapper.classList.remove("nav-close");
 };
+
+// Adds closed class to nav bar and removes open class
 const closeNav = () => {
     wrapper.classList.remove("nav-open");
     wrapper.classList.add("nav-close");
@@ -62,3 +72,13 @@ const closeNav = () => {
 function displayMenu() {
     navIsOpen() ? closeNav() : openNav();
 };
+
+//Add event listener to toggle button
+burgerMenu.addEventListener('click', displayMenu);
+
+//Add event listener to nav buttons
+menuItems.forEach(
+    function(menuItem) {
+        menuItems.addEventListener("click", displayMenu);
+    }
+);
